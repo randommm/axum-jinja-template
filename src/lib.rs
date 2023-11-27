836c5053671f3492dc550eaefb3c51bd9acf1c86
Marco Inacio <m@marcoinacio.com>
2023-11-27 20:55:15 +0000

    update Axum to 0.7

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 mod routes;
+use tokio::net::TcpListener;
 
 pub async fn run() -> Result<(), String> {
     let app = routes::create_routes().await?;
-    let bind_addr = &"0.0.0.0:3511"
-        .parse()
-        .map_err(|e| format!("Failed to parse address: {}", e))?;
-    axum::Server::bind(bind_addr)
-        .serve(app.into_make_service())
+    let bind_addr = &"0.0.0.0:3511";
+    let listener = TcpListener::bind(bind_addr)
         .await
-        .map_err(|e| format!("Server error: {}", e))
+        .map_err(|e| format!("Failed to parse address: {}", e))?;
+    axum::serve(listener, app.into_make_service())
+        .await
+        .map_err(|e| format!("Server error: {}", e))?;
+    Ok(())
 }
